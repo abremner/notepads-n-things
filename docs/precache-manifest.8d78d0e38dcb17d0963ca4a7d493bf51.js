fc9a3236c1ac9d58d64fc96f007fc5e164231461
Angus Bremner <angusbremner@gmail.com>
2018-10-02 22:44:57 +1000

    add build folder

---
+++ docs/precache-manifest.8d78d0e38dcb17d0963ca4a7d493bf51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "89cad5c0127454973c73",
+    "url": "/static/js/main.89cad5c0.chunk.js"
+  },
+  {
+    "revision": "0970b1d1883bb3eb6794",
+    "url": "/static/js/1.0970b1d1.chunk.js"
+  },
+  {
+    "revision": "89cad5c0127454973c73",
+    "url": "/static/css/main.cc3f6733.chunk.css"
+  },
+  {
+    "revision": "6c379e736e820664a2d91d8d30f7b534",
+    "url": "/index.html"
+  }
+];
